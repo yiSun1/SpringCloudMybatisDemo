f42567699f831926db85042a8b45c3f215695240
zhangxs7 <zhangxs7@lenovo.com>
2017-06-22 17:07:53 +0800

    test
    Signed-off-by: zhangxs7 <zhangxs7@lenovo.com>

---
+++ dao/src/main/java/com/demo/dao/BankSettleBillFlowMapper.java
@@ -0,0 +1,24 @@
+package com.demo.dao;
+
+import com.demo.common.PageModel;
+import com.demo.common.PageQuery;
+import com.demo.domain.BankSettleBillFlow;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.Map;
+
+public interface BankSettleBillFlowMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(BankSettleBillFlow record);
+
+    int insertSelective(BankSettleBillFlow record);
+
+    BankSettleBillFlow selectByPrimaryKey(@Param("id")Integer id);
+
+    int updateByPrimaryKeySelective(BankSettleBillFlow record);
+
+    int updateByPrimaryKey(BankSettleBillFlow record);
+
+    PageModel<BankSettleBillFlow> getPage(PageQuery pageQuery,Map paramsMap);
+}
