5906343241d5e83a4d4ae6755f83ef15e4c74266
zhangxs7 <zhangxs7@lenovo.com>
2017-06-26 11:46:37 +0800

    add transaction support

--- manager/src/main/java/com/demo/manager/impl/bankBillSettleManagerImpl.java
+++ manager/src/main/java/com/demo/manager/impl/bankBillSettleManagerImpl.java
@@ -2,11 +2,11 @@ package com.demo.manager.impl;
 
 import com.demo.common.PageModel;
 import com.demo.common.PageQuery;
+import com.demo.common.RemoteResult;
 import com.demo.dao.BankSettleBillFlowMapper;
 import com.demo.domain.BankSettleBillFlow;
 import com.demo.manager.bankBillSettleManager;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.annotation.ManagedBean;
 import java.util.Map;
@@ -15,7 +15,6 @@ import java.util.Map;
  * Created by zhangxs7 on 2017/6/22.
  */
 @ManagedBean
-@EnableTransactionManagement
 public class bankBillSettleManagerImpl implements bankBillSettleManager {
 
     @Autowired
@@ -29,5 +28,19 @@ public class bankBillSettleManagerImpl implements bankBillSettleManager {
     @Override
     public PageModel<BankSettleBillFlow> getBank2(PageQuery pageQuery, Map map) {
         return bankSettleBillFlowMapper.getPage(pageQuery,map);
+    }
+
+    @Override
+    public RemoteResult<Boolean> insertTransaction(BankSettleBillFlow bankSettleBillFlow){
+        RemoteResult<Boolean> result=new RemoteResult<Boolean>(false);
+        BankSettleBillFlow bb=bankSettleBillFlowMapper.selectByPrimaryKey(31);
+        bb.setId(29);
+        int re=bankSettleBillFlowMapper.insertSelective(bb);
+        if(re>0){
+            result.setSuccess(true);
+           String tt=null;
+            tt.trim();
+        }
+        return result;
     }
 }
