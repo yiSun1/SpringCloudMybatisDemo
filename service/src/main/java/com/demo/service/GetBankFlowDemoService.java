2cd1f149e0d4311697d71abc99be0760053fdbc7
zhangxs7 <zhangxs7@lenovo.com>
2017-06-26 12:20:54 +0800

    add api model 1

---
+++ service/src/main/java/com/demo/service/GetBankFlowDemoService.java
@@ -0,0 +1,10 @@
+package com.demo.service;
+
+/**
+ * Created by zhangxs7 on 2017/6/26.
+ */
+public interface GetBankFlowDemoService {
+    public String user();
+    public String user2();
+    public String transactionTest();
+}
